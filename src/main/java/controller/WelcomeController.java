f47a72d3e404a03f16a73a3a611d49cb9dd8b5d3
MagnusHannibal <158312079+MagnusHannibal@users.noreply.github.com>
2024-09-24 10:52:21 +0200

    pom fix + html file

--- src/main/java/controller/WelcomeController.java
+++ src/main/java/controller/WelcomeController.java
@@ -1,11 +1,12 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WelcomeController {
 
-    @GetMapping("/get") public String home() {
+    @GetMapping("/get") public String sayWelcome() {
         return "Welcome";
     }
 }
