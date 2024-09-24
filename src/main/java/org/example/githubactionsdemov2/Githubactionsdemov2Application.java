d1c5f1a2d37f0ef7a236d661397b26bb3dafc1d4
MagnusHannibal <158312079+MagnusHannibal@users.noreply.github.com>
2024-09-24 10:27:53 +0200

    Initial commit

---
+++ src/main/java/org/example/githubactionsdemov2/Githubactionsdemov2Application.java
@@ -0,0 +1,13 @@
+package org.example.githubactionsdemov2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Githubactionsdemov2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Githubactionsdemov2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/githubactionsdemov2/Githubactionsdemov2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.githubactionsdemov2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Githubactionsdemov2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
